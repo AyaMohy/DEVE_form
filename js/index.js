6ca9074a6853f90573e9d5f1ba3f81f0dc693999
AyaMohy <aya.mohey162@gmail.com>
2022-07-13 16:02:25 +0200

    editing back button

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
+// code for second and third option
 function doneTab(no){
     document.querySelector(`.selectedCard_claim_${no}`).style.display='none'
     document.querySelector(`.selectedCard_done_${no}`).style.display='block'
 }
 function hideAllTabs(no){
-  document.querySelector(`.selectedCard_claim_${no}`).style.display='block';
-  window.event.target.parentNode.style.display='none'
+    // document.querySelectorAll('.tab')[0].style.display='block'
+    // document.querySelectorAll('.tab')[1].style.display='none'
+    // document.getElementById("nextBtn").style.display = "none";
+  document.querySelector(`.selectedCard_claim_${no}`).style.display='none';
+  window.event.target.parentNode.style.display='none';
+//   document.querySelector('.firstTab').style.display='block'
+}
+function showFirstTab(no){
+    document.querySelector(`.selectedCard_claim_${no}`).style.display='block';
+
 }
 
-function hide_All_Tabs(){
-    var x = document.getElementsByClassName("tab_2");
-    for(let i=0; i<x.length-1;i++){
-        x[i].style.display='none'
-    }
-    
-}
+
+
+
+
