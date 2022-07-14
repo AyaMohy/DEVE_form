ca5f4fa0e04e9fa4df08f6863cd19ab5bc06c44c
AyaMohy <aya.mohey162@gmail.com>
2022-07-14 02:59:13 +0200

    editing copyLink component

--- components/CopyLink.js
+++ components/CopyLink.js
@@ -1,30 +1,26 @@
-
+const template = document.createElement('template');
+template.innerHTML=
+    `
+        <div class="copiedWrapper">
+            <p class="copiedLink px-2"> </p>
+            <button class="iconCopy px-2"><slot name="copyIcon"> </slot> </button>
+        </div>
+    `;
 
 class CopyLink extends HTMLElement{
     constructor(){
         super();
         const shadow = this.attachShadow({mode: 'open'});
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('class','input-block')
-        const copiedLink=document.createElement('p')
-        copiedLink.textContent=this.getAttribute('linkAddress')
-        copiedLink.setAttribute('class', 'copiedLink')
-        const copyIcon=document.createElement('slot')
-        copyIcon.setAttribute('name', 'copyIcon')
-        const button=document.createElement('button')
-        button.appendChild(copyIcon)
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('p').innerText = this.getAttribute('linkAddress');
+
 
        
         
         const styleElem = document.createElement('link');
         styleElem.setAttribute('rel', 'stylesheet');
         styleElem.setAttribute('href', './css/style.css');
-
-        // attach the created element to the shadow Dom
-        shadow.appendChild(wrapper);
         shadow.appendChild(styleElem)
-        wrapper.appendChild(copiedLink);
-        wrapper.appendChild(button);
        
     }
 
@@ -36,7 +32,8 @@ class CopyLink extends HTMLElement{
 
     connectedCallback() {
         this.shadowRoot.querySelector('button').addEventListener('click', () => this.copyToClipboard());
-      }
+       
+    }
      
 }
 
